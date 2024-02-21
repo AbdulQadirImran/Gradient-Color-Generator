d27dd97e898c3698e9a59bb29cee7dcb5e3c2807
AbdulQadirImran <160231143+AbdulQadirImran@users.noreply.github.com>
2024-02-21 19:03:58 +0500

    script.js

--- script.js
+++ script.js
@@ -33,7 +33,6 @@ colorInputs.forEach(input => {
     // Calling generateGradient function on each color input clicks
     input.addEventListener("input", () => generateGradient(false));
 });
-
 selectMenu.addEventListener("change", () => generateGradient(false));
 refreshBtn.addEventListener("click", () => generateGradient(true));
 copyBtn.addEventListener("click", copyCode);
